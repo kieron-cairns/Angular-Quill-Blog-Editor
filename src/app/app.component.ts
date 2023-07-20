daab1dfa94f3cc7595679e223c7d7bb3d0cc86a4
Kieron Cairns <kieroncairns@protonmail.com>
2023-07-20 18:39:52 +0100

    CHANGE: Post HTML content to API endpoint implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { QuillEditorComponent } from 'ngx-quill';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,9 @@ export class AppComponent {
 
   content: string = ''
 
-  // ...
+  // Inject HttpClient in the constructor
+  constructor(private http: HttpClient) {}
+
 
   editorCreated($event: any) {
     const toolbar = $event.getModule('toolbar');
@@ -22,7 +26,17 @@ export class AppComponent {
   exportContentToHTML() {
     const editorContentAsHTML = this.editor.quillEditor.root.innerHTML;
     console.log(editorContentAsHTML);
-    // Now you can do whatever you want with editorContentAsHTML
+
+    const postApiEndpointUrl = '';
+
+    const payload = {
+      content: editorContentAsHTML
+    };
+
+    this.http.post(postApiEndpointUrl, payload)
+    .subscribe(res => {
+      console.log(res);//
+    });
   }
 
 
